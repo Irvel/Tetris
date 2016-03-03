47b0ca373c44f673bc84766e543e58953a8b11c7
Irvel <irvel.nduva@gmail.com>
2016-03-02 21:20:09 -0600

    Refactor paintComponent functions in smaller subfunctions

--- src/tetris/SidePanel.java
+++ src/tetris/SidePanel.java
@@ -14,90 +14,86 @@ public class SidePanel extends JPanel {
 	/**
 	 * Serial Version UID.
 	 */
-	private static final long LSERIALVERSIONUID = 2181495598854992747L;
+	private static final long lSERIALVERSIONUID = 2181495598854992747L;
 
 	/**
 	 * The dimensions of each tile on the next piece preview.
 	 */
-	private static final int ITILE_SIZE = BoardPanel.ITILE_SIZE >> 1;
+	private static final int iTILE_SIZE = BoardPanel.iTILE_SIZE >> 1;
 	
 	/**
 	 * The width of the shading on each tile on the next piece preview.
 	 */
-	private static final int ISHADE_WIDTH = BoardPanel.ISHADE_WIDTH >> 1;
+	private static final int iSHADE_WIDTH = BoardPanel.iSHADE_WIDTH >> 1;
 	
 	/**
 	 * The number of rows and columns in the preview window. Set to
 	 * 5 because we can show any piece with some sort of padding.
 	 */
-	private static final int ITILE_COUNT = 5;
+	private static final int iTILE_COUNT = 5;
 	
 	/**
 	 * The center x of the next piece preview box.
 	 */
-	private static final int ISQUARE_CENTER_X = 130;
+	private static final int iSQUARE_CENTER_X = 130;
 	
 	/**
 	 * The center y of the next piece preview box.
 	 */
-	private static final int ISQUARE_CENTER_Y = 65;
+	private static final int iSQUARE_CENTER_Y = 65;
 	
 	/**
 	 * The size of the next piece preview box.
 	 */
-	private static final int ISQUARE_SIZE = (ITILE_SIZE * ITILE_COUNT >> 1);
+	private static final int iSQUARE_SIZE = (iTILE_SIZE * iTILE_COUNT >> 1);
 	
 	/**
 	 * The number of pixels used on a small insets (generally used for categories).
 	 */
-	private static final int ISMALL_INSET = 20;
+	private static final int iSMALL_INSET = 20;
 	
 	/**
 	 * The number of pixels used on a large insets.
 	 */
-	private static final int ILARGE_INSET = 40;
+	private static final int iLARGE_INSET = 40;
 	
 	/**
 	 * The y coordinate of the stats category.
 	 */
-	private static final int ISTATS_INSET = 140;
+	private static final int iSTATS_INSET = 140;
 	
 	/**
 	 * The y coordinate of the controls category.
 	 */
-	private static final int ICONTROLS_INSET = 240;
+	private static final int iCONTROLS_INSET = 240;
 	
 	/**
 	 * The number of pixels to offset between each string.
 	 */
-	private static final int ITEXT_STRIDE = 25;
+	private static final int iTEXT_STRIDE = 25;
 	
 	/**
 	 * The small font.
 	 */
-	private static final Font FSMALL_FONT = new Font("Dialog", Font
+	private static final Font fonSMALL_FONT = new Font("Dialog", Font
 			.BOLD, 11);
 	
 	/**
 	 * The large font.
 	 */
-	private static final Font FLARGE_FONT = new Font("Dialog", Font.BOLD,
-													 13);
+	private static final Font fonLARGE_FONT = new Font("Dialog", Font.BOLD,
+													   13);
 	
 	/**
 	 * The color to draw the text and preview box in.
 	 */
-	private static final Color CDRAW_COLOR = new Color(173, 255, 255);
+	private static final Color colDRAW_COLOR = new Color(173, 255, 255);
 	
 	/**
 	 * The Tetris instance.
 	 */
 	private Tetris tetTetris;
-        
-        /**
-         * Background image
-         */
-        private Image imgBackground;
+
 	
 	/**
 	 * Creates a new SidePanel and sets it's display properties.
@@ -106,56 +102,35 @@ public class SidePanel extends JPanel {
 	public SidePanel(Tetris tetTetris) {
 		this.tetTetris = tetTetris;
 		
-		setPreferredSize(new Dimension(200, BoardPanel.PANEL_HEIGHT));
+		setPreferredSize(new Dimension(200, BoardPanel.iPANEL_HEIGHT));
 		setBackground(Color.BLACK);
 	}
-	
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		//Set the color for drawing.
-		g.setColor(CDRAW_COLOR);
-		
-		/*
-		 * This variable stores the current y coordinate of the string.
-		 * This way we can re-order, add, or remove new strings if necessary
-		 * without needing to change the other strings.
-		 */
-		int offset;
-		
-		/*
-		 * Draw the "Stats" category.
-		 */
-		g.setFont(FLARGE_FONT);
-		g.drawString("Stats", ISMALL_INSET, offset = ISTATS_INSET);
-		g.setFont(FSMALL_FONT);
-		g.drawString("Level: " + tetTetris.getLevel(), ILARGE_INSET, offset += ITEXT_STRIDE);
-		g.drawString("Score: " + tetTetris.getScore(), ILARGE_INSET, offset += ITEXT_STRIDE);
-		
-		/*
-		 * Draw the "Controls" category.
-		 */
-		g.setFont(FLARGE_FONT);
-		g.drawString("Controls", ISMALL_INSET, offset = ICONTROLS_INSET);
-		g.setFont(FSMALL_FONT);
-		g.drawString("A - Move Left", ILARGE_INSET, offset += ITEXT_STRIDE);
-		g.drawString("D - Move Right", ILARGE_INSET, offset += ITEXT_STRIDE);
-		g.drawString("Q - Rotate Anticlockwise", ILARGE_INSET, offset += ITEXT_STRIDE);
-		g.drawString("E - Rotate Clockwise", ILARGE_INSET, offset += ITEXT_STRIDE);
-		g.drawString("S - Drop", ILARGE_INSET, offset += ITEXT_STRIDE);
-		g.drawString("P - Pause Game", ILARGE_INSET, offset += ITEXT_STRIDE);
-		g.drawString("G - Save Game", ILARGE_INSET, offset += ITEXT_STRIDE);
-		g.drawString("C - Load Game", ILARGE_INSET, offset += ITEXT_STRIDE);
-		g.drawString("T - Stop/Start Track", ILARGE_INSET, offset +=
-				ITEXT_STRIDE);
 
+	@Override
+	public void paintComponent(Graphics graGraphics) {
+		super.paintComponent(graGraphics);
+		//Set the color for drawing.
+		graGraphics.setColor(colDRAW_COLOR);
 		/*
-		 * Draw the next piece preview box.
-		 */
-		g.setFont(FLARGE_FONT);
-		g.drawString("Next Piece:", ISMALL_INSET, 70);
-		g.drawRect(ISQUARE_CENTER_X - ISQUARE_SIZE, ISQUARE_CENTER_Y - ISQUARE_SIZE, ISQUARE_SIZE * 2, ISQUARE_SIZE * 2);
-		
+     	 * Draw the information of the side panel
+     	 */
+		drawPreviewPiece(graGraphics);
+		drawStats(graGraphics);
+		drawControls(graGraphics);
+	}
+
+	/**
+	 * Draws the next piece preview at the top of the side panel
+	 * @param graGraphics The Graphics object to be drawn to.
+	 */
+	private void drawPreviewPiece(Graphics graGraphics) {
+    	/*
+     	 * Draw the next piece preview box.
+     	 */
+		graGraphics.setFont(fonLARGE_FONT);
+		graGraphics.drawString("Next Piece:", iSMALL_INSET, 70);
+		graGraphics.drawRect(iSQUARE_CENTER_X - iSQUARE_SIZE, iSQUARE_CENTER_Y - iSQUARE_SIZE, iSQUARE_SIZE * 2, iSQUARE_SIZE * 2);
+
 		/*
 		 * Draw a preview of the next piece that will be spawned. The code is pretty much
 		 * identical to the drawing code on the board, just smaller and centered, rather
